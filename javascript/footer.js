763f0f1d444d315b6fdce83d7ded8b22972342c3
Victoria <vima0001@stud.kea.dk>
2023-12-13 19:33:52 +0100

    footer indsættelse

---
+++ javascript/footer.js
@@ -0,0 +1,15 @@
+// Tilmeld knap
+function skjulTekst() {
+  // Hent mail-inputfeltet ved hjælp af dets id
+  var mailInput = document.getElementById("mail");
+
+  // Tildel tom streng til værdien af mail-inputfeltet
+  mailInput.value = "";
+}
+function skjulTekst() {
+  // Hent mail-inputfeltet ved hjælp af dets id
+  var mailInput = document.getElementById("mail2");
+
+  // Tildel tom streng til værdien af mail-inputfeltet
+  mailInput.value = "";
+}
